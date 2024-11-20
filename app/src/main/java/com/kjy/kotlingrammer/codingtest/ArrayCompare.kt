e50de0cd07b732c2a83e4a56ef40714de7107075
JaeYoun Kim <rlawodusx@naver.com>
2024-11-21 03:01:11 +0900

    Feat: 코딩테스트 - 배열 비교

---
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/ArrayCompare.kt
@@ -0,0 +1,22 @@
+package com.kjy.kotlingrammer.codingtest
+
+class Solution {
+    fun solution(arr1: IntArray, arr2: IntArray): Int {
+        var answer: Int = 0
+        when {
+            arr1.size != arr2.size ->  {
+                answer = if(arr1.size > arr2.size) 1 else -1
+            }
+            arr1.size == arr2.size ->  {
+                answer = if(arr1.sum() == arr2.sum()) {
+                    0
+                }else if(arr1.sum() > arr2.sum()) {
+                    1
+                } else {
+                    -1
+                }
+            }
+        }
+        return answer
+    }
+}
