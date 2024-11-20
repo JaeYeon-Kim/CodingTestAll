96b40a019b6fa23fa7d4058534c084df0a0fbc4b
JaeYoun Kim <rlawodusx@naver.com>
2024-11-20 22:35:28 +0900

    코딩테스트: 마지막 두원소, 이진변환반복

---
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/FinalTwoElement.kt
@@ -0,0 +1,27 @@
+package com.kjy.kotlingrammer.codingtest
+
+///*
+//*
+//* 정수 리스트 num_list가 주어질 때,
+//* 마지막 원소가 그전 원소보다 크면 마지막 원소에서 그전 원소를 뺀 값을 마지막 원소가 그전 원소보다 크지 않다면 마지막 원소를 두 배한 값을 추가하여 return하도록 solution 함수를 완성해주세요.
+//* */
+//class Solution {
+//    fun solution(num_list: IntArray): IntArray {
+//
+//        val resultList = mutableListOf<Int>()
+//        num_list.forEach {
+//            resultList.add(it)
+//        }
+//
+//        val lastNumber = resultList.last()
+//        val lastMinusOneNumber = resultList[resultList.lastIndex - 1]
+//
+//        if (lastNumber > lastMinusOneNumber) {
+//            resultList.add(lastNumber - lastMinusOneNumber)
+//        } else {
+//            resultList.add(lastNumber * 2)
+//        }
+//
+//        return resultList.toIntArray()
+//    }
+//}
--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -2,40 +2,39 @@ package com.kjy.kotlingrammer.codingtest
 
 fun main() {
     val solve = Solution()
-    val result = solve.solution("abc1abc1abc")
-    println(result)
+    println(solve.solution("110010101001").joinToString())
 }
 
 
-/*
-* 문자열 code가 주어집니다.
-code를 앞에서부터 읽으면서 만약 문자가 "1"이면 mode를 바꿉니다. mode에 따라 code를 읽어가면서 문자열 ret을 만들어냅니다.
-mode는 0과 1이 있으며, idx를 0 부터 code의 길이 - 1 까지 1씩 키워나가면서 code[idx]의 값에 따라 다음과 같이 행동합니다.
-* */
-
-class Solution {
-    fun solution(code: String): String {
-        var ret: String = ""
-        var mode: Int = 0
-
-        code.forEachIndexed { idx, value ->
-            when (mode) {
-                0 -> if ((code[idx] != '1') && (idx % 2 == 0)) {
-                    ret += code[idx]
-                } else if (code[idx] == '1') {
-                    mode = 1
-                }
-
-                1 -> if (code[idx] != '1' && (idx % 2 == 1)) {
-                    ret += code[idx]
-                } else if (code[idx] == '1') {
-                    mode = 0
-                }
-            }
-        }
-        return ret.ifBlank { "EMPTY" }
-    }
-}
+///*
+//* 문자열 code가 주어집니다.
+//code를 앞에서부터 읽으면서 만약 문자가 "1"이면 mode를 바꿉니다. mode에 따라 code를 읽어가면서 문자열 ret을 만들어냅니다.
+//mode는 0과 1이 있으며, idx를 0 부터 code의 길이 - 1 까지 1씩 키워나가면서 code[idx]의 값에 따라 다음과 같이 행동합니다.
+//* */
+//
+//class Solution {
+//    fun solution(code: String): String {
+//        var ret: String = ""
+//        var mode: Int = 0
+//
+//        code.forEachIndexed { idx, value ->
+//            when (mode) {
+//                0 -> if ((code[idx] != '1') && (idx % 2 == 0)) {
+//                    ret += code[idx]
+//                } else if (code[idx] == '1') {
+//                    mode = 1
+//                }
+//
+//                1 -> if (code[idx] != '1' && (idx % 2 == 1)) {
+//                    ret += code[idx]
+//                } else if (code[idx] == '1') {
+//                    mode = 0
+//                }
+//            }
+//        }
+//        return ret.ifBlank { "EMPTY" }
+//    }
+//}
 
 
 
