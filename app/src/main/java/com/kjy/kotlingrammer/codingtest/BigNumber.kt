b94c97a003995be068d85fc43f0e754540f8c6a0
JaeYoun Kim <rlawodusx@naver.com>
2024-11-20 23:13:29 +0900

    Feat:코딩테스트 - 가장 큰수

---
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/BigNumber.kt
@@ -0,0 +1,11 @@
+package com.kjy.kotlingrammer.codingtest
+
+class Solution {
+    fun solution(array: IntArray): IntArray {
+        var answer: IntArray = intArrayOf()
+        array.forEachIndexed { index, value ->
+            if(value == array.maxOf { it }) answer = intArrayOf(value, index)
+        }
+        return answer
+    }
+}
--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -2,7 +2,8 @@ package com.kjy.kotlingrammer.codingtest
 
 fun main() {
     val solve = Solution()
-    println(solve.solution("110010101001").joinToString())
+    val result = solve.solution(intArrayOf(1, 8, 3))
+    println(result.joinToString())
 }
 
 
