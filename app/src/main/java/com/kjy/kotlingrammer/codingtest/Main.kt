0f49aaede1a1352d3e1c9ddf5fdecee22f60dbdd
JaeYoun Kim <rlawodusx@naver.com>
2024-11-24 18:44:11 +0900

    Feat: 코딩테스트 - 수정

--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -1,29 +1,46 @@
 package com.kjy.kotlingrammer.codingtest
 
 fun main() {
-    val solve = Solution()
-   
+    val rect1 = Rectangle(10, 5)
+    val rect2 = Rectangle(3, 8)
+    println(rect1 + rect2)
 }
 
-class Solution {
-    fun solution(numLog: IntArray): String {
-        var result = ""
-        run breaker@ {
-            numLog.forEachIndexed { idx, num ->
-                if(numLog.lastIndex == idx) return@breaker
-                when(numLog[idx] + numLog[idx + 1]) {
-                    1 -> result += "w"
-                    -1 -> result += "s"
-                    10 -> result += "d"
-                    -10 -> result += "a"
-                }
-            }
-        }
-        return result
+interface Plusable<T> {
+    operator fun plus(other: T): T
+}
+
+class Rectangle(val width: Int, val height: Int) : Plusable<Rectangle> {
+    override fun plus(other: Rectangle): Rectangle {
+        return Rectangle(width + other.width, height + other.height)
     }
+
+    override fun toString(): String {
+        return "width: $width, height: $height"
+    }
+
 }
 
 
+//class Solution {
+//    fun solution(numLog: IntArray): String {
+//        var result = ""
+//        run breaker@{
+//            numLog.forEachIndexed { idx, num ->
+//                if (numLog.lastIndex == idx) return@breaker
+//                when (numLog[idx] + numLog[idx + 1]) {
+//                    1 -> result += "w"
+//                    -1 -> result += "s"
+//                    10 -> result += "d"
+//                    -10 -> result += "a"
+//                }
+//            }
+//        }
+//        return result
+//    }
+//}
+//
+//
 
 
 
