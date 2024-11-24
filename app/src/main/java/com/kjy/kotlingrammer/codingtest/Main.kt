a3fb5731aaae708be22a6b1e4549eca07703c11d
JaeYoun Kim <rlawodusx@naver.com>
2024-11-24 16:49:14 +0900

    Feat: 코딩테스트
    문자열 입력 변환

--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -2,38 +2,26 @@ package com.kjy.kotlingrammer.codingtest
 
 fun main() {
     val solve = Solution()
-    println(solve.solution(123))
+   
 }
 
 class Solution {
-    fun solution(n: Int): Int {
-       var sum = 0
-       n.toString().forEach {
-           sum += it.code
-       }
-       return sum
+    fun solution(numLog: IntArray): String {
+        var result = ""
+        run breaker@ {
+            numLog.forEachIndexed { idx, num ->
+                if(numLog.lastIndex == idx) return@breaker
+                when(numLog[idx] + numLog[idx + 1]) {
+                    1 -> result += "w"
+                    -1 -> result += "s"
+                    10 -> result += "d"
+                    -10 -> result += "a"
+                }
+            }
+        }
+        return result
     }
 }
-
-
-//class Solution {
-//    fun solution(v: Array<IntArray>): IntArray {
-//
-//        val xList = mutableListOf<Int>()
-//        val yList = mutableListOf<Int>()
-//
-//        v.forEach {
-//            xList.add(it[0])
-//            yList.add(it[1])
-//        }
-//
-//        val resultX = xList.groupBy { it }.filter { array -> array.value.size < 2 }.keys.first()
-//        val resultY = yList.groupBy { it }.filter { array -> array.value.size < 2 }.keys.first()
-//
-//        return intArrayOf(resultX, resultY)
-//    }
-//}
-
 
 
 
