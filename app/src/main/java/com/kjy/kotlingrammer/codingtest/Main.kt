87cc88db20ed167fc6c34f026eb2117f48886cff
JaeYoun Kim <rlawodusx@naver.com>
2024-11-25 22:17:32 +0900

    Feat: 코딩테스트 푸는 도중 임시저장

--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -1,51 +1,27 @@
 package com.kjy.kotlingrammer.codingtest
 
 fun main() {
-    val rect1 = Rectangle(10, 5)
-    val rect2 = Rectangle(3, 8)
-    println(rect1 + rect2)
+    val solve = Solution()
+    val result = solve.solution(29183,1)
+    println(result)
 }
 
-interface Plusable<T> {
-    operator fun plus(other: T): T
-}
 
-class Rectangle(val width: Int, val height: Int) : Plusable<Rectangle> {
-    override fun plus(other: Rectangle): Rectangle {
-        return Rectangle(width + other.width, height + other.height)
+class Solution {
+    fun solution(num: Int, k: Int): Int {
+        var answer = 0
+        var test = "dfAA"
+        test.lowercase()
+
+        num.toString().toList().forEachIndexed { idx, value ->
+            if(value.toString().toInt() == k) {
+                answer = idx
+            }
+        }
+
+        return answer
     }
-
-    override fun toString(): String {
-        return "width: $width, height: $height"
-    }
-
 }
-
-
-//class Solution {
-//    fun solution(numLog: IntArray): String {
-//        var result = ""
-//        run breaker@{
-//            numLog.forEachIndexed { idx, num ->
-//                if (numLog.lastIndex == idx) return@breaker
-//                when (numLog[idx] + numLog[idx + 1]) {
-//                    1 -> result += "w"
-//                    -1 -> result += "s"
-//                    10 -> result += "d"
-//                    -10 -> result += "a"
-//                }
-//            }
-//        }
-//        return result
-//    }
-//}
-//
-//
-
-
-
-
-
 
 
 
