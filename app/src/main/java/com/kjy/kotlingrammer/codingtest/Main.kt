7998ff7edb46ea255d7926b17a01f6135333a118
JaeYoun Kim <rlawodusx@naver.com>
2024-11-26 18:51:01 +0900

    Feat: 코딩테스트 추가

--- app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
+++ app/src/main/java/com/kjy/kotlingrammer/codingtest/Main.kt
@@ -1,27 +1,34 @@
 package com.kjy.kotlingrammer.codingtest
 
 fun main() {
-    val solve = Solution()
-    val result = solve.solution(29183,1)
+    val testString = "hello world"
+    var result = ""
+    testString.forEach {
+        result += it
+    }
+
     println(result)
 }
 
 
-class Solution {
-    fun solution(num: Int, k: Int): Int {
-        var answer = 0
-        var test = "dfAA"
-        test.lowercase()
-
-        num.toString().toList().forEachIndexed { idx, value ->
-            if(value.toString().toInt() == k) {
-                answer = idx
-            }
-        }
-
-        return answer
-    }
-}
+//class Solution {
+//    fun solution(binomial: String): Int {
+//        var answer: Int = 0
+//        var input = binomial
+//        var wildCardArray = arrayOf<String>("+", "-", "*")
+//        wildCardArray.forEach { wildCard ->
+//            if(binomial.contains(wildCard)) {
+//                val numberList = input.replace(" ", "").split(wildCard)
+//                when(wildCard) {
+//                    "+" -> answer = numberList[0].toInt() + numberList[1].toInt()
+//                    "-" -> answer = numberList[0].toInt() - numberList[1].toInt()
+//                    "*" -> answer = numberList[0].toInt() * numberList[1].toInt()
+//                }
+//            }
+//        }
+//        return answer
+//    }
+//}
 
 
 
